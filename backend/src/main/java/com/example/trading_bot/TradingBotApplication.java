63b085e46389263cfbff295c660d0c0c5fcd4929
heysep <121409727+heysep@users.noreply.github.com>
2025-08-12 15:12:41 +0900

    chore: docker/env 설정 업데이트 및 backend 추가

--- backend/src/main/java/com/example/trading_bot/TradingBotApplication.java
+++ backend/src/main/java/com/example/trading_bot/TradingBotApplication.java
@@ -2,6 +2,16 @@ package com.example.trading_bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+<<<<<<< HEAD
+
+@SpringBootApplication
+public class TradingBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradingBotApplication.class, args);
+    }
+
+=======
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -12,4 +22,5 @@ public class TradingBotApplication {
     public static void main(String[] args) {
         SpringApplication.run(TradingBotApplication.class, args);
     }
+>>>>>>> 2af5e9b0255480fee23e71c69bf06f9111819120
 }
